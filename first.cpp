db33c0f88f084be801cf9b1f69f8aa014f64dd11
Sourish Chandra <kisuke@PolishGirl.kali>
2025-05-15 14:12:54 +0530

    Describe your changes

---
+++ first.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  /*
+          Sha sha ooh! Bing Chilling! Wo jyao Anita Max Wynn. Wo fay-chang
+     shee-hwan bing-chee-lin.
+  */
+  cout << "hello there my fellow aliens" << endl;
+  cout << "i am anita max wynn" << endl;
+  cout << "I am from dholakpur" << endl;
+  cout << "hello there my fellow aliens" << endl;
+  cout << "i am anita max wynn" << endl;
+  cout << "I am from dholakpur" << endl;
+  cout << "hello there my fellow aliens" << endl;
+  cout << "i am anita max wynn" << endl;
+  cout << "I am from dholakpur" << endl;
+  cout << "hello there my fellow aliens" << endl;
+  cout << "i am anita max wynn" << endl;
+  cout << "I am from dholakpur" << endl;
+  cout << "hello there my fellow aliens" << endl;
+  cout << "i am anita max wynn" << endl;
+  cout << "I am from dholakpur" << endl;
+  cout << "hello there my fellow aliens" << endl;
+  cout << "i am anita max wynn" << endl;
+  cout << "I am from dholakpur" << endl;
+  cout << "hello there my fellow aliens" << endl;
+  cout << "i am anita max wynn" << endl;
+  cout << "I am from dholakpur" << endl;
+  cout << "hello there my fellow aliens" << endl;
+  cout << "i am anita max wynn" << endl;
+  cout << "I am from dholakpur" << endl;
+  cout << "hello there my fellow aliens" << endl;
+  cout << "i am anita max wynn" << endl;
+  cout << "I am from dholakpur" << endl;
+  cout << "hello there my fellow aliens" << endl;
+  cout << "i am anita max wynn" << endl;
+  cout << "I am from dholakpur" << endl;
+  cout << "hello there my fellow aliens" << endl;
+  cout << "i am anita max wynn" << endl;
+  cout << "I am from dholakpur" << endl;
+  // return 0;
+}
---
+++ learning.md
@@ -0,0 +1,11 @@
+# Path: -
+
+- Day1: -
+  - 1st one(in the beninging):-
+    - entry point -> **int main(),
+    - **iostream -> provides input and output as stream as c++ doem't have it (noob)
+    - printed my details 10 times
+  - 2nd one(comment):-
+    - // one line comment
+    - /* */ more than one line of comment (can't do nesting of comment throws error.
+  - 3rd one(oh no moment error):-
